597073441df283bf747ec906a39c74da137442d3
Raushan132 <raushankumar132abc@gmail.com>
2023-06-04 09:30:02 +0530

    add framer motion and compose mail ui

---
+++ src/components/activity/Compose.jsx
@@ -0,0 +1,28 @@
+import {AiOutlineClose} from 'react-icons/ai'
+import { motion } from "framer-motion"
+
+const Compose = ({composeVisible}) => {
+    
+    return (
+        <motion.div className='fixed right-16 bottom-10 w-96 bg-base-200'
+        initial={{ y: '150vw' }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.5 }}
+        >
+            <div className='flex justify-between px-2 py-2 '>
+                <div>New Message</div>
+                <div className='cursor-pointer' onClick={composeVisible} ><AiOutlineClose /></div>
+            </div>
+            <form>
+                <div className='flex flex-col justify-start gap-2 px-2 '>
+                    <div><input type='text' placeholder='To' className='w-full pl-2 bg-base-200 border-0 border-b-2 border-gray-500' /></div>
+                    <div><input type='text' placeholder='Subject' className='w-full pl-2 bg-base-200 border-0 border-b-2 border-gray-500'  /></div>
+                    <div><textarea placeholder='Write your message' className='w-full pl-2 h-72 resize-none  bg-base-200 my-2' /></div>
+                    <div className='flex justify-end px-8 py-2'><button className=' btn-primary px-8 py-2 text-sm  rounded-full'>Send</button></div>
+                </div>
+            </form>
+        </motion.div>
+    )
+}
+
+export default Compose
--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { BsFillPencilFill, BsFillInboxFill, BsFillSendFill } from 'react-icons/bs'
+import Compose from '../../components/activity/Compose'
+import { useState } from 'react'
 
 const Messages = () => {
+
+  const[composeVisible,setComposeVisible]=useState(false);
+
+  const handleComposeVisible =()=>{
+      setComposeVisible(false)
+  }
+
   return (
-    <div>Messages</div>
+    <>
+      <div>
+        {/* activities like compose btn inbox btn and sent btn */}
+        <div className='flex gap-5'>
+
+          <button className='btn flex gap-2' onClick={()=>setComposeVisible(true)}>
+            <div><BsFillPencilFill /> </div>
+            Compose
+          </button>
+          <button className='btn flex gap-2'>
+            <div><BsFillInboxFill /></div>
+            Inbox
+            <div className="">+99</div>
+          </button>
+          <button className='btn flex gap-2'>
+            <div ><BsFillSendFill /> </div>
+            Sent
+          </button>
+        </div>
+
+    
+    {
+      
+     composeVisible &&   <div> <Compose composeVisible={handleComposeVisible} />  </div>
+    }
+
+
+      </div>
+
+
+
+
+
+
+    </>
+
   )
 }
 
